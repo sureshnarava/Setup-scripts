46f1e6e968982a35f0e59bd2723d19d8ef84ad3a
sureshnarava <sureshbabunarava@gmail.com>
2025-08-14 10:36:11 +0100

    Create trivy.sh

---
+++ trivy.sh
@@ -0,0 +1,7 @@
+#steps to install trivy:
+wget https://github.com/aquasecurity/trivy/releases/download/v0.18.3/trivy_0.18.3_Linux-64bit.tar.gz
+tar zxvf trivy_0.18.3_Linux-64bit.tar.gz
+sudo mv trivy /usr/local/bin/
+#Vim .bashrc
+#export PATH=$PATH:/usr/local/bin/
+#source .bashrc 
